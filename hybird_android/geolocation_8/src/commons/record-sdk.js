08307392cea52bd05835bb4ce47df530cc7d3f3c
yoyo <chenzhifang@bejohns.com>
2019-04-25 23:52:59 +0800

    新增Android录音代码

--- hybird_android/geolocation_8/src/commons/record-sdk.js
+++ hybird_android/geolocation_8/src/commons/record-sdk.js
@@ -1,39 +1,29 @@
-import Recorder from "./Recorder";
 export default class Record {
     startRecord(param) {
-        let self = this;
         try {
-            Recorder.get(rec => {
-                console.log("init recorder component now.");
-                self.recorder = rec;
-                self.recorder.start();
-                console.log("start record now.");
-                param.success("record successfully!");
-            });
+            console.log("Recording start now.");
+            window.startVoice = () => {
+                console.log("start voice now.");
+                param.success();
+                window.startVoice = undefined;
+            };
+            jsCallback.startVoice();
         } catch (e) {
-            param.error("record failed!" + e);
+            param.error("Error:" + e);
         }
     }
 
     stopRecord(param) {
-        console.log("stop record now.");
-        let self = this;
         try {
-            let blobData = self.recorder.getBlob();
-            param.success(blobData);
+            console.log("Recording stop now.");
+            window.stopVoice = (maxDB) => {
+                console.log("stop voice now:" + maxDB);
+                param.success(maxDB);
+                window.stopVoice = undefined;
+            };
+            jsCallback.stopVoice();
         } catch (e) {
-            param.error("record stop failed!" + e);
-        }
-    }
-
-    play(audio) {
-        console.log("start play record now.");
-        let self = this;
-        try {
-            self.recorder.play(audio);
-            console.log("play successfully.");
-        } catch (e) {
-            console.log("play record failed!" + e);
+            param.error("Error:" + e);
         }
     }
 }
--- hybird_android/geolocation_8/src/components/Record2.vue
+++ hybird_android/geolocation_8/src/components/Record2.vue
@@ -3,6 +3,11 @@
 		<div class="record-title">Click following button to record voice:</div>
 		<input @click="startRecord" type="button" value="录音">
 		<input @click="stopRecord" type="button" value="停止">
+		<input @click="play" type="button" value="播放">
+		<div class="record-play" v-show="isFinished">
+			<div class="record-title">Current voice player is:</div>
+			<audio controls autoplay></audio>
+		</div>
 	</div>
 </template>
 
@@ -12,6 +17,8 @@
 		name: "Record",
 		data() {
 			return {
+				isFinished: false,
+				audio: "",
 				recorder: new Record()
 			};
 		},
@@ -19,6 +26,7 @@
 			startRecord: function() {
 				console.log("start to record now.");
 				let self = this;
+				self.isFinished = false;
 				self.recorder.startRecord({
 					success: res => {
 						console.log("start record successfully.");
@@ -31,14 +39,23 @@
 			stopRecord: function() {
 				console.log("stop record now.");
 				let self = this;
+				self.isFinished = false;
 				self.recorder.stopRecord({
 					success: res => {
+						//此处可以获取音频源文件(res)，用于上传等操作
 						console.log("stop record successfully.");
 					},
 					error: res => {
 						console.log("stop record failed.");
 					}
 				});
+			},
+			play: function() {
+				console.log("play record now.");
+				let self = this;
+				self.isFinished = true;
+				self.audio = document.querySelector("audio");
+				self.recorder.play(self.audio);
 			}
 		}
 	};
